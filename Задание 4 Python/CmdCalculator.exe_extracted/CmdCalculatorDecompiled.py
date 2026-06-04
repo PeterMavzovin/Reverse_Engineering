8071302c950217955c690f584531d6786271cdbc
Petr5 <petr.mavzovin@mail.ru>
2026-06-04 11:34:30 +0300

    Change Python version to 3.7. Decompiled application

--- Задание 4 Python/CmdCalculator.exe_extracted/CmdCalculatorDecompiled.py
+++
@@ -1,7 +0,0 @@
-# uncompyle6 version 3.9.3
-# Python bytecode version base 3.10 (3439)
-# Decompiled from: Python 3.10.0b1 (tags/v3.10.0b1:ba42175, May  3 2021, 20:22:30) [MSC v.1928 64 bit (AMD64)]
-# Embedded file name: CmdCalculator.py
-
-Unsupported Python version, 3.10, for decompilation
-
